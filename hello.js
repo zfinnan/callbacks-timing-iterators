b716ca2d0aa3b5615bdeee613aae5cf7000756c4
zfinnan <63361320+zfinnan@users.noreply.github.com>
2020-10-22 09:59:33 -0400

    [js] Writing next five

--- hello.js
+++ hello.js
@@ -32,3 +32,33 @@ function sayHello(name) {
 }
 
 console.log(sayHello('number FIVE'));
+
+function sayHello(name) {
+    let msg = 'Hello, ' + name + '. How are you?';
+    return msg;
+}
+console.log(sayHello('number SIX'));
+
+function sayHello(name) {
+    let msg = 'Hello, ' + name + '. How are you?';
+    return msg;
+}
+console.log(sayHello('number SEVEN'));
+
+function sayHello(name) {
+    let msg = 'Hello, ' + name + '. How are you?';
+    return msg;
+}
+console.log(sayHello('number EIGHT'));
+
+function sayHello(name) {
+    let msg = 'Hello, ' + name + '. How are you?';
+    return msg;
+}
+console.log(sayHello('number NINE'));
+
+function sayHello(name) {
+    let msg = 'Hello, ' + name + '. How are you?';
+    return msg;
+}
+console.log(sayHello('number TEN'));
