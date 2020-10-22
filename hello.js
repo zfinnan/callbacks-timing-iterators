29a354d9b6bf7bfc19d988d4b52d9a1f077a6293
zfinnan <63361320+zfinnan@users.noreply.github.com>
2020-10-22 09:53:45 -0400

    Writing 5 times

---
+++ hello.js
@@ -0,0 +1,34 @@
+function sayHello(name) {
+    let msg = 'Hello, ' + name + '. How are you?';
+    return msg;
+}
+
+console.log(sayHello('number ONE'));
+
+function sayHello(name) {
+    let msg = 'Hello, ' + name + '. How are you?';
+    return msg;
+}
+
+console.log(sayHello('number TWO'));
+
+function sayHello(name) {
+    let msg = 'Hello, ' + name + '. How are you?';
+    return msg;
+}
+
+console.log(sayHello('number THREE'));
+
+function sayHello(name) {
+    let msg = 'Hello, ' + name + '. How are you?';
+    return msg;
+}
+
+console.log(sayHello('number FOUR'));
+
+function sayHello(name) {
+    let msg = 'Hello, ' + name + '. How are you?';
+    return msg;
+}
+
+console.log(sayHello('number FIVE'));
