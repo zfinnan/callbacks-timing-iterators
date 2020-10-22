22624e4aee4b96ea2efec18298474f6aedba35e4
zfinnan <63361320+zfinnan@users.noreply.github.com>
2020-10-22 10:08:25 -0400

    [js] Writing checkNumber five times

---
+++ checkNumber.js
@@ -0,0 +1,59 @@
+function checkNumber(num) {
+    if (num > 0) {
+        return 'positive';
+    } else if (num < 0) {
+        return 'negative';
+    } else {
+        return 'zero';
+    }
+}
+
+console.log(checkNumber(5)); 
+
+function checkNumber(num) {
+    if (num > 0) {
+        return 'positive';
+    } else if (num < 0) {
+        return 'negative';
+    } else {
+        return 'zero';
+    }
+}
+
+console.log(checkNumber(-5));
+
+function checkNumber(num) {
+    if (num > 0) {
+        return 'positive';
+    } else if (num < 0) {
+        return 'negative';
+    } else {
+        return 'zero';
+    }
+}
+
+console.log(checkNumber(6));
+
+function checkNumber(num) {
+    if (num > 0) {
+        return 'positive';
+    } else if (num < 0) {
+        return 'negative';
+    } else {
+        return 'zero';
+    }
+}
+
+console.log(checkNumber(-6));
+
+function checkNumber(num) {
+    if (num > 0) {
+        return 'positive';
+    } else if (num < 0) {
+        return 'negative';
+    } else {
+        return 'zero';
+    }
+}
+
+console.log(checkNumber(0));
